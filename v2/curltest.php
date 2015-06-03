e54c4c69e4e8c02e9d3c5f54920b36e14f8143ff
Roberto <roberto.bartolome.medina@gmail.com>
2015-06-03 17:56:09 +0200

    wip

--- v2/curltest.php
+++ v2/curltest.php
@@ -6,8 +6,7 @@ $fields = array('action'=>'get');
 
 $postArgs = array('data' => serialize($fields));
 
-$url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v1/index.php";
-//$url = "http://localhost/alumniapi/v2/index.php";
+$url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v2/index.php";
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST, 1);
 curl_setopt($ch,CURLOPT_POSTFIELDS, $postArgs);
@@ -16,33 +15,8 @@ curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
 curl_setopt($ch,CURLOPT_TIMEOUT, 20);
 
 $response = curl_exec($ch);
-var_dump($response);
 
-//break;
-
-
-/*
-$conf = json_decode(file_get_contents('configuration.json'), TRUE);
-$db = new mysqli($conf["host"], $conf["user"], $conf["password"], $conf["database"]); // development
-
-foreach ($res->data as $params)
-{
-	$query = 'INSERT INTO alumni_personal_test ('
-	. 'titles'
-	. ', firstname'
-	. ', surname'
-	. ', irb_surname'
-	. ' )'
-	. ' VALUES ('
-	. '\'' . $params->titles .'\''
-	. ', \'' . $params->firstname .'\''
-	. ', \'' . $params->surname .'\''
-	. ', \'' . $params->irb_surname .'\''
-	. ' )'
-	;
-	$result = $db->query($query);
-}
-*/
-
+$res = json_decode($response);
+var_dump($res);
 
 ?>
