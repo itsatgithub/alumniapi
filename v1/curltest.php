b6e7606f76302f6eb7ee08871a54601b01a1cac8
Roberto <roberto.bartolome.medina@gmail.com>
2015-06-03 17:28:18 +0200

    wip

--- v1/curltest.php
+++ v1/curltest.php
@@ -6,8 +6,8 @@ $fields = array('action'=>'get');
 
 $postArgs = array('data' => serialize($fields));
 
-//$url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v1/index.php";
-$url = "http://localhost/alumniapi/v1/index.php";
+$url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v1/index.php";
+//$url = "http://localhost/alumniapi/v1/index.php";
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST, 1);
 curl_setopt($ch,CURLOPT_POSTFIELDS, $postArgs);
