72e52c48c75fe9e411b6c796c9d5e9a525ac065a
Roberto <roberto.bartolome.medina@gmail.com>
2015-06-04 13:29:31 +0200

    wip

--- v2/curltest.php
+++ v2/curltest.php
@@ -11,7 +11,7 @@ $url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v2/index.php";
 //$url = "http://localhost/alumniapi/v2/index.php";
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST, 1);
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $postArgs);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
 curl_setopt($ch,CURLOPT_TIMEOUT, 20);
