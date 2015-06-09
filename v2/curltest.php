71376ce5668b6574346d6ac6b92a44715574a887
Roberto <roberto.bartolome.medina@gmail.com>
2015-06-09 15:25:53 +0200

    wip

--- v2/curltest.php
+++ v2/curltest.php
@@ -1,24 +1,63 @@
 <?php
 
-$ch = curl_init();
+$fields = Array
+(
+    "action" => "save",
+    "alumni_personalcode" => "",
+    "titles" => "0000000000003",
+    "firstname" => "prueba3",
+    "surname" => "Magan",
+    "irb_surname" => "",
+    "gender" => "00001",
+    "nationality" => "AFG",
+    "nationality_2" => "AUT",
+    "birth" => "1961-11-18",
+    "email" => "fermon+123456789112221@gmail.com",
+    "url" => "http://www.google.com",
+    "facebook" => "http://www.facebook.com",
+    "linkedin" => "http://www.linkedin.com",
+    "twitter" => "fermon83",
+    "keywords" => "",
+    "biography" => "bio",
+    "awards" => "awa",
+    "ORCIDID" => "342432",
+    "researcherid" => "423432",
+    "pubmedid" => "342432",
+    "show_data" => "1",
+    "external_jobs" => Array
+        (
+            0 => Array
+                (
+                    "start_date" => "2015-03-10",
+                    "end_date" => "2015-06-25",
+                    "external_job_positions" => "0000000000005",
+                    "comments" => "",
+                    "external_job_sectors" => "0000000000003",
+                    "institution" => "",
+                    "address" => "",
+                    "postcode" => "",
+                    "city" => "",
+                    "country" => "AT",
+                    "current" => "1"
+                )
 
-$fields = array('action'=>'get');
-//$fields = array('action'=>'get', 'alumni_personalcode' => '00034');
+        )
 
-$postArgs = array('data' => serialize($fields));
+);
+
+//print_r($fields);
+//break;
 
 $url = "http://alumniapitest.irbbarcelona.pcb.ub.es/v2/index.php";
-//$url = "http://localhost/alumniapi/v2/index.php";
 curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_POST, 1);
-curl_setopt($ch,CURLOPT_POSTFIELDS, $postArgs);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+curl_setopt($ch,CURLOPT_POST, 1);  
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+//curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
-print_r($response);
-//$res = json_decode($response);
-//var_dump($res);
+echo $response;
+
+$res = json_decode($response);
+echo $res;
 
 ?>
